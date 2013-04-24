a6b9cbcc14c9563f4ddd4be707608c9db6fa105e
song <song@song-PC>
2013-04-24 22:18:53 +0800

    增加过滤器，用于鉴权

---
+++ src/com/filter/AdminVerifyFilter.java
@@ -0,0 +1,45 @@
+package com.filter;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class AdminVerifyFilter implements Filter
+{
+    @Override
+    public void destroy()
+    {
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp,
+            FilterChain chain) throws IOException, ServletException
+    {
+        req.setCharacterEncoding("utf-8");
+        HttpServletRequest request = (HttpServletRequest)req;
+        HttpSession hs = request.getSession();
+        String login = (String) hs.getAttribute("login");
+        if(login==null || !login.equals("admin"))
+        {
+            HttpServletResponse response = (HttpServletResponse)resp;
+            response.sendRedirect("/onlineTest/login.jsp");
+            return;
+        }
+        chain.doFilter(req, resp);
+    }
+
+    @Override
+    public void init(FilterConfig config) throws ServletException
+    {
+    }
+
+}
