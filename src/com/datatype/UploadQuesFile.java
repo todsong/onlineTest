7f662ee1b87bc5bb18d61a860ed3c68cf7653076
song <song@song-PC>
2013-05-07 20:13:11 +0800

    用于描述批量导入的文件

---
+++ src/com/datatype/UploadQuesFile.java
@@ -0,0 +1,25 @@
+package com.datatype;
+
+import java.io.File;
+
+public class UploadQuesFile
+{
+    private int subjectId;
+    private File file;
+    public int getSubjectId()
+    {
+        return subjectId;
+    }
+    public void setSubjectId(int subjectId)
+    {
+        this.subjectId = subjectId;
+    }
+    public File getFile()
+    {
+        return file;
+    }
+    public void setFile(File file)
+    {
+        this.file = file;
+    }
+}
